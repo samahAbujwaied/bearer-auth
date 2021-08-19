3b324a5189ebe94e6e3da8e43be3b6bb4e00b01c
‘samahAbujwaied’ <samah.abujweed@yahoo.com>
2021-08-19 20:39:58 +0300

    last edit

--- starter-code/src/auth/models/index.js
+++ starter-code/src/auth/models/index.js
@@ -3,7 +3,7 @@
 require('dotenv').config();
 const { Sequelize, DataTypes } = require('sequelize');
 const userModel = require('./users')
-const DATABASE_URL = process.env.DATABASE_URL || 'postgres://jdyeacbnararzc:924da0cf67214a917e2f58127094c00e523f5817c2c821e8f91b2c35f6a43fd3@ec2-54-155-61-133.eu-west-1.compute.amazonaws.com:5432/d9c5j7817ui0fl'
+const DATABASE_URL = process.env.DATABASE_URL || 'postgres://localhost:5432/user'
 let sequelizeOptions = {
     dialectOptions: {
         ssl: {
